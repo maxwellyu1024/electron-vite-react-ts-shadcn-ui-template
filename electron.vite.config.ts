53f34038eefca6a4e9c04349d1d5958be334bfad
abcijkxyz <abcijkxyz@163.com>
2025-05-21 17:51:43 +0800

    shadcn

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,6 +2,8 @@ import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()]
@@ -12,9 +14,10 @@ export default defineConfig({
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@': resolve('src/renderer/src')
       }
     },
-    plugins: [react()]
+    plugins: [react(), tailwindcss()]
   }
 })
